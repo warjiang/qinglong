f7c3d6f514f72770591773f1b6eb7c376ddde0b8
dingwenjiang <dingwenjiang@bytedance.com>
2022-04-12 00:44:49 +0800

    feat: publicPath调整

--- .umirc.ts
+++ .umirc.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   },
   favicon: '/images/g5.ico',
   publicPath: "/ql/",
+  runtimePublicPath: true,
   proxy: {
     '/api/public': {
       target: 'http://127.0.0.1:5400/',
